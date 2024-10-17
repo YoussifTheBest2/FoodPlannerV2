f8b0f9f31ed793c32bc2d2c6fbac95913d022b92
YoussifTheBest2 <youssiftameradam@gmail.com>
2024-10-17 23:14:55 +0300

    Added Calories
    added Calories

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask , render_template , request , redirect
+from flask import Flask , render_template , request , redirect 
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+import random as rand
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foodplanner.db'
 db = SQLAlchemy(app)
+
+
+
 
 class BreakfastM(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -40,12 +44,12 @@ class DinnerM(db.Model):
 def index():
     if request.method == 'POST':
         items_to_add = []
-
+        
         try:
             # Access form data using correct field names (ensure they match your HTML form)
             brItemContent = request.form['brContent']
             brItemCount = request.form['brCount']
-            new_brItem = BreakfastM(content=brItemContent,count = brItemCount)
+            new_brItem = BreakfastM(content=brItemContent,count = brItemCount,calories = rand.randint(50,400))
             items_to_add.append(new_brItem)  # Add successfully created Breakfast item
         except KeyError:
             pass  # Ignore missing 'brContent' field
@@ -53,7 +57,7 @@ def index():
         try:
             luItemContent = request.form['luContent']
             luItemCount = request.form['luCount']
-            new_luItem = LunchM(content=luItemContent,count=luItemCount)  # Use LunchM for lunch items
+            new_luItem = LunchM(content=luItemContent,count=luItemCount,calories = rand.randint(50,400))  # Use LunchM for lunch items
             items_to_add.append(new_luItem)  # Add successfully created Lunch item
         except KeyError:
             pass  # Ignore missing 'luContent' field
@@ -61,7 +65,7 @@ def index():
         try:
             diItemContent = request.form['diContent']
             diItemCount = request.form['diCount']
-            new_diItem = DinnerM(content=diItemContent,count=diItemCount)  # Use DinnerM for dinner items
+            new_diItem = DinnerM(content=diItemContent,count=diItemCount,calories = rand.randint(50,400))  # Use DinnerM for dinner items
             items_to_add.append(new_diItem)  # Add successfully created Dinner item
         except KeyError:
             pass  # Ignore missing 'diContent' field
@@ -83,6 +87,10 @@ def index():
         brItems = BreakfastM.query.order_by(BreakfastM.id).all()
         luItems = LunchM.query.order_by(LunchM.id).all()
         diItems = DinnerM.query.order_by(DinnerM.id).all()
+
+        
+
+        
 
         # Pass all items to the template for display
         return render_template("index.html", brItems=brItems, luItems=luItems, diItems=diItems)
@@ -107,6 +115,7 @@ def delete(id,meal):
     except Exception as e:
         print(f"Error deleting item: {e}")
         return "There was an issue deleting your item."
+
 
 
 
